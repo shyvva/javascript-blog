a9075bfd558dc90ffdd908823f994cb1e0abf7e4
Dawid Przydryga <shivva90@gmail.com>
2020-03-10 21:36:43 +0100

    script.js update, removed active class from selectors.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -8,11 +10,19 @@ const titleClickHandler = function (event) {
     console.log(event);
 
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
     /* add class 'active' to the clicked link */
 
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post');
 
+    for  (let activeArticle of activeArticles) {
+        activeArticle.classList.remove('active')
+    }
     /* get 'href' attribute from the clicked link */
 
     /* find the correct article using the selector (value of 'href' attribute) */
