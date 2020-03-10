eb9035899c81bd5f3552f92ce9674473798e367a
Dawid Przydryga <shivva90@gmail.com>
2020-03-10 21:59:14 +0100

    Add class active to clicked links

--- js/script.js
+++ js/script.js
@@ -6,21 +6,25 @@
   }); */
 
 const titleClickHandler = function (event) {
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
+
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
+    /*[IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
+    /*[DONE] remove class 'active' from all articles */
+
     const activeArticles = document.querySelectorAll('.post');
 
-    for  (let activeArticle of activeArticles) {
+    for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active')
     }
     /* get 'href' attribute from the clicked link */
