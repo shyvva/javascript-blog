6fb354736800eb071589a2c112b7d8465961304f
Dawid Przydryga <shivva90@gmail.com>
2020-03-11 21:10:15 +0100

    Add js to sidebar

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
   }); */
 
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -16,7 +17,7 @@ const titleClickHandler = function (event) {
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-    /*[IN PROGRESS] add class 'active' to the clicked link */
+    /*[DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
 
@@ -25,13 +26,22 @@ const titleClickHandler = function (event) {
     const activeArticles = document.querySelectorAll('.post');
 
     for (let activeArticle of activeArticles) {
-        activeArticle.classList.remove('active')
+        activeArticle.classList.remove('active');
     }
-    /* get 'href' attribute from the clicked link */
-
-    /* find the correct article using the selector (value of 'href' attribute) */
-
-    /* add class 'active' to the correct article */
+    /*[DONE] get 'href' attribute from the clicked link */
+   
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log (articleSelector);
+   
+    /*[DONE] find the correct article using the selector (value of 'href' attribute) */
+    
+    const articleTarget = document.querySelector(articleSelector);
+    console.log (articleTarget);
+    
+    /*[DONE] add class 'active' to the correct article */
+    
+    articleTarget.classList.add('active');
+    console.log('clickedElement:', articleTarget);
 }
 
 const links = document.querySelectorAll('.titles a');
