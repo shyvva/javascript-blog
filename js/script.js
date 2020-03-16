2b750b82894f90c344a08e18543de5484fffc8e3
Dawid Przydryga <shivva90@gmail.com>
2020-03-16 19:36:31 +0100

    Add JS to logo

--- js/script.js
+++ js/script.js
@@ -5,6 +5,61 @@
     console.log('links:', links);
   }); */
 {
+    let TxtRotate = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtRotate.prototype.tick = function () {
+        let i = this.loopNum % this.toRotate.length;
+        let fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        let that = this;
+        let delta = 300 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function () {
+        let elements = document.getElementsByClassName('txt-rotate');
+        for (let i = 0; i < elements.length; i++) {
+            let toRotate = elements[i].getAttribute('data-rotate');
+            let period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtRotate(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+
+    }
+
+
+
     const titleClickHandler = function (event) {
         event.preventDefault();
         const clickedElement = this;
@@ -56,7 +111,9 @@
 
         const titleList = document.querySelector(optTitleListSelector);
         console.log(titleList);
+
         /* for each article */
+
         const articles = document.querySelectorAll(optArticleSelector);
 
         let html = '';
@@ -74,12 +131,14 @@
             /* [DONE] get the title from the title element */
 
             const getTitle = article.getElementsByTagName('h3');
+
             /* [DONE] create HTML of the link */
 
             const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
             console.log(linkHTML);
 
             /* [DONE] insert link into titleList */
+
             html = html + linkHTML;
             console.log(html);
 
@@ -87,10 +146,10 @@
         titleList.innerHTML = html;
     }
     generateTitleLinks();
-    
+
     const links = document.querySelectorAll('.titles a');
     console.log(links);
-        for (let link of links) {
-            link.addEventListener('click', titleClickHandler);
-        }
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
 }
